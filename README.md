6922be578256bdfbeb209f0e1857c80bbc24a0e6
Jordy3D <fchetar@gmail.com>
2023-11-09 00:10:47 +1100

    Made more template friendly
    - Split up style from "base"
    - Added notes to some parts
    - Cleaned up code and CSS
    - Added README

---
+++ README.md
@@ -0,0 +1,19 @@
+# tree
+
+Ready-to-use JSON-based tree of links for use in bios, descriptions, etc.
+
+## About
+
+This project is designed to make collecting together links to projects, socials, etc. easier.  
+The HTML and CSS are designed to be as minimal as possible, so that you can easily add your own styling.  
+
+## Usage
+
+To use this, either click on the Use This Template button, or fork the repository.  
+AFter that, you can edit the `tree.json` file to add your own links.  
+You can also edit the `index.html` file and/or the `style.css` file to add your own styling.  
+
+## Attribution  
+
+Inspired somewhat by Linktree, though it's not exactly like a list of links in one place is all that unique.  
+I'd appreciate it if you left the link to this repository in the footer, but you don't strictly have to.
--- js/script.js
+++ js/script.js
@@ -1,25 +1,23 @@
-var linkDiv = `<a class="link" href="URL" target="_blank">
-            <div class="link-image">
-                <img src="IMAGE_URL" alt="IMAGE_ALT">
-            </div>
-            <div class="link-title">TITLE</div>
-            <div class="link-description">DESCRIPTION</div>
-        `;
-var userDiv = `<div class="user">
-            <div class="user-image">
-                <img src="IMAGE_URL" alt="IMAGE_ALT">
-            </div>
-            <div class="user-name">NAME</div>
-            <div class="user-description">DESCRIPTION</div>
-        </div>`;
+// TEMPLATE DEFINITIONS
+
+const linkDiv = `
+<a class="link" href="URL" target="_blank">
+    <div class="link-image">
+        <img src="IMAGE_URL" alt="IMAGE_ALT">
+    </div>
+    <div class="link-title">TITLE</div>
+    <div class="link-description">DESCRIPTION</div>
+`;
+const userDiv = `
+<div class="user">
+    <div class="user-image">
+        <img src="IMAGE_URL" alt="IMAGE_ALT">
+    </div>
+    <div class="user-name">NAME</div>
+    <div class="user-description">DESCRIPTION</div>
+</div>`;
 
 loadData();
-
-
-// loadData();
-
-// loadUser();
-// loadLinks();
 
 function loadData() {
     console.log('Loading Data');
@@ -35,75 +33,56 @@ function loadData() {
 }
 
 function loadUser(data) {
-    console.log('Loading User');
-
-    // use the userDiv template
     var userHTML = userDiv;
-    // replace the placeholders with the user data
+
     userHTML = userHTML.replace('IMAGE_URL', data.image);
     userHTML = userHTML.replace('IMAGE_ALT', data.name);
     userHTML = userHTML.replace('NAME', data.name);
     userHTML = userHTML.replace('DESCRIPTION', data.description);
-    // add the user to the page
+
     document.querySelector('.user-info').innerHTML += userHTML;
 }
 
 function loadLinks(data) {
-    console.log('Loading Links');
-
     var linksContainer = document.querySelector('.links');
-
     var links = data.links;
-
     var groups = [];
 
+    // create groups
     links.forEach(link => {
-        // add a new group if it doesn't exist
         if (!groups.includes(link.group)) {
             groups.push(link.group);
 
-            // if the group is not empty
-            if (link.group != '') {
-                var groupHTML = `<div class="group group-${link.group}"><h2>${link.group}</h2></div>`;
-            }
-            else {
+            if (link.group != '')
+                var groupHTML = `<div class="group group-${link.group}"><h2 class="group-header">${link.group}</h2></div>`;
+            else
                 var groupHTML = `<div class="group group-main"></div>`;
-            }
-            linksContainer.innerHTML += groupHTML;
 
+            linksContainer.innerHTML += groupHTML;
         }
     });
 
-
-    // for each link
+    // create links
     links.forEach(link => {
-        // use the linkDiv template
         var linkHTML = linkDiv;
-        // replace the placeholders with the link data
 
         linkHTML = linkHTML.replace('URL', link.url);
 
         linkHTML = linkHTML.replace('IMAGE_URL', link.image);
         linkHTML = linkHTML.replace('IMAGE_ALT', link.title);
-        // if there is no image, hide it
         if (link.image == '')
             linkHTML = linkHTML.replace('link-image', 'link-image hide');
 
         linkHTML = linkHTML.replace('TITLE', link.title);
-        // if there's no description, make title take up the whole space
-        if (link.description == '')
+        if (link.description == '') // if there's no description, make title take up the whole space
             linkHTML = linkHTML.replace('link-title', 'link-title link-title-expand');
 
         linkHTML = linkHTML.replace('DESCRIPTION', link.description);
 
-        // add the link to the page in the right group, or in the main group if there is no group
-        if (link.group == '') {
+        if (link.group == '')
             document.querySelector('.group-main').innerHTML += linkHTML;
-        } else {
+        else
             document.querySelector(`.group-${link.group}`).innerHTML += linkHTML;
-        }
-
-
     });
 
 }
